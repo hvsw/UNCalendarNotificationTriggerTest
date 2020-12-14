8fb8cebec29a00be53a29d141ab7d9aece05c7d7
Henrique Valcanaia <henriqueindalencio+github@gmail.com>
2020-12-14 17:23:53 -0300

    First commit

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -6,15 +6,55 @@
 //  Copyright © 2020 Henrique Valcanaia. All rights reserved.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        let c: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
+//        let d1 = Date()
+//        
+//        let a = Calendar.current.dateComponents(c, from: d1)
+//        print(a)
+//        
+//        let secondsFromGMT = TimeZone.current.secondsFromGMT()
+//        print(secondsFromGMT)
+//        let d2 = d1.addingTimeInterval(TimeInterval(secondsFromGMT))
+//        let b = Calendar.current.dateComponents(c, from: d2)
+//        print(b)
     }
-
-
+    
+    @IBAction func tap(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.body = "body"
+        content.sound = .default
+        content.threadIdentifier = "thread id"
+        content.userInfo = [:]
+        
+        let secondsFromGMT = TimeInterval(TimeZone.current.secondsFromGMT())
+        print(secondsFromGMT)
+        
+        let fireDateCaralho = Date().addingTimeInterval(2)
+        print(fireDateCaralho)
+        
+        let fireDateDateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
+                                                                     from: fireDateCaralho)
+        print(fireDateDateComponents)
+        
+        let calendarTrigger = UNCalendarNotificationTrigger(dateMatching: fireDateDateComponents,
+                                                            repeats: false)
+        
+        let notificationRequest = UNNotificationRequest(identifier: content.threadIdentifier,
+                                                        content: content,
+                                                        trigger: calendarTrigger)
+        
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if let e = error {
+                print(e)
+            }
+        }
+    }
 }
-
